13e837dd08ebcf2c372b9b141186b29abc143ec4
fran-cisc <130452833+fran-cisc@users.noreply.github.com>
2023-04-30 12:48:00 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,30 +9,44 @@ radio.onReceivedNumber(function (receivedNumber) {
             `)
         radio.sendNumber(3)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (myGuess == 0 && receivedNumber == 1) {
         basic.showIcon(IconNames.No)
         radio.sendNumber(4)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (myGuess == 0 && receivedNumber == 2) {
         basic.showIcon(IconNames.Yes)
         radio.sendNumber(5)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (myGuess == 1 && receivedNumber == 0) {
         basic.showIcon(IconNames.Yes)
         radio.sendNumber(5)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (myGuess == 1 && receivedNumber == 2) {
         basic.showIcon(IconNames.No)
         radio.sendNumber(4)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (myGuess == 2 && receivedNumber == 0) {
         basic.showIcon(IconNames.No)
         radio.sendNumber(4)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (myGuess == 2 && receivedNumber == 1) {
         basic.showIcon(IconNames.Yes)
         radio.sendNumber(5)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (receivedNumber == 3) {
         basic.showLeds(`
             . . . . .
@@ -42,12 +56,18 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             `)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (receivedNumber == 4) {
         basic.showIcon(IconNames.Yes)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else if (receivedNumber == 5) {
         basic.showIcon(IconNames.No)
         myGuess = 9
+        basic.pause(500)
+        basic.clearScreen()
     } else {
     	
     }
@@ -56,19 +76,37 @@ input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     radio.sendNumber(0)
     myGuess = 0
-    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . # # # .
+        . # # # .
+        . # # # .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     radio.sendNumber(1)
     myGuess = 1
-    basic.showIcon(IconNames.SmallSquare)
+    basic.showLeds(`
+        . # . . .
+        # # . . .
+        # # # # .
+        # # . . .
+        . # . . .
+        `)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.clearScreen()
     radio.sendNumber(2)
     myGuess = 2
-    basic.showIcon(IconNames.Scissors)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . # .
+        . # # . .
+        # . # . .
+        `)
 })
 let myGuess = 0
 radio.setGroup(777)
