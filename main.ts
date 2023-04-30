d1945948a3002508632259271fdf39de96ba7d59
fran-cisc <130452833+fran-cisc@users.noreply.github.com>
2023-04-30 12:32:25 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,74 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (myGuess == receivedNumber) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        radio.sendNumber(3)
+        myGuess = 9
+    } else if (myGuess == 0 && receivedNumber == 1) {
+        basic.showIcon(IconNames.No)
+        radio.sendNumber(4)
+        myGuess = 9
+    } else if (myGuess == 0 && receivedNumber == 2) {
+        basic.showIcon(IconNames.Yes)
+        radio.sendNumber(5)
+        myGuess = 9
+    } else if (myGuess == 1 && receivedNumber == 0) {
+        basic.showIcon(IconNames.Yes)
+        radio.sendNumber(5)
+        myGuess = 9
+    } else if (myGuess == 1 && receivedNumber == 2) {
+        basic.showIcon(IconNames.No)
+        radio.sendNumber(4)
+        myGuess = 9
+    } else if (myGuess == 2 && receivedNumber == 0) {
+        basic.showIcon(IconNames.No)
+        radio.sendNumber(4)
+        myGuess = 9
+    } else if (myGuess == 2 && receivedNumber == 1) {
+        basic.showIcon(IconNames.Yes)
+        radio.sendNumber(5)
+        myGuess = 9
+    } else if (receivedNumber == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        myGuess = 9
+    } else if (receivedNumber == 4) {
+        basic.showIcon(IconNames.Yes)
+        myGuess = 9
+    } else if (receivedNumber == 5) {
+        basic.showIcon(IconNames.No)
+        myGuess = 9
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    radio.sendNumber(0)
+    myGuess = 0
+    basic.showIcon(IconNames.Square)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendNumber(1)
+    myGuess = 1
+    basic.showIcon(IconNames.SmallSquare)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    radio.sendNumber(2)
+    myGuess = 2
+    basic.showIcon(IconNames.Scissors)
+})
+let myGuess = 0
+radio.setGroup(777)
